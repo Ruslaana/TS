9ccdff4dfe07844495a6794fb6c3bbe8dcbc6b5c
Ruslana Onyshchuk <lalelya7@gmail.com>
2023-09-15 19:06:50 +0200

    some exercises

---
+++ main.ts
@@ -0,0 +1,34 @@
+// interface User {
+//   name: string;
+//   id: number;
+// }
+
+// const user: User = {
+//   name: 'Ruslana',
+//   id: 0,
+// }
+
+// class UserAccount {
+//   name: string;
+//   id: number;
+
+//   constructor(name: string, id: number) {
+//     this.name = name;
+//     this.id = id;
+//   }
+// }
+
+// const userInfo: User = new UserAccount('Ruslana', 1)
+
+// type Customer = {
+//   birthday: Date
+// }
+
+// function getCustomer (id: number): Customer | null | undefined {
+//   return id === 0 ? null : { birthday: new Date() }
+// }
+
+// let customer = getCustomer(0);
+// // optional property access operator
+// console.log(customer?.birthday.getFullYear);
+
